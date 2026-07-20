f8f6d0c26b76146f373a915b11723d1c5f654ec6
abdelrahman220033 <abdelrahmansameh932@gmail.com>
2026-07-20 22:10:04 +0300

    fix: move validation pipe above app.listen

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4001);
+  
   app.useGlobalPipes(new ValidationPipe());
+  
+  await app.listen(process.env.PORT ?? 4001);
 }
 bootstrap();
